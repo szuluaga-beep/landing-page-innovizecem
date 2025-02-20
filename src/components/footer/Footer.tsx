5e743354153bf12054310b1beeafbbe4d13bf296
Jhonier Mosquera <jhoniermosquera61@gmail.com>
2025-02-20 15:20:22 -0500

    add(cofiguración basica de proyecto, creación de carpetas y en este commit, se ignora la internacionalización para finalizr su configuración)

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import style from "./Footer.module.css"
+
+const Footer = () => {
+  return (
+    <>
+      <div>
+          <p>Hola2</p>
+      </div>
+      <div className={style.downfooter}>
+          <p>2025 Innova cem</p>
+      </div>
+    </>
+  )
+}
+
+export default Footer
