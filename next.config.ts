eb6144147db93223376e091981ade938230d705c
szuluaga-beep <steven000069@hotmail.com>
2025-06-03 16:16:44 -0500

    fix: update image configuration to use remotePatterns for Cloudinary

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
-import {NextConfig} from 'next';
+import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const nextConfig: NextConfig = {
     images: {
-        domains: ["res.cloudinary.com"]
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                pathname: '/images/**',
+            },]
     }
 };
- 
+
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
